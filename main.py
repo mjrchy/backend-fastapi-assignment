95807ef2139601e12a3d8731d3177512c75e190f
Jiratchaya Thongsuthum <92805190+mjrchy@users.noreply.github.com>
2023-02-12 11:35:25 +0700

    edit method

--- main.py
+++ main.py
@@ -46,35 +46,44 @@ def get_reservation_by_name(name:str):
     if type(name) !=  str:
         raise HTTPException(400, "Name should be string")
     reservation_list = []
-    for item in collection.find({"name": name}):
+    for item in collection.find({"name": name}, {"_id": False}):
         reservation_list.append(item)
     return {"result": reservation_list}
 
 @app.get("/reservation/by-room/{room_id}")
 def get_reservation_by_room(room_id: int):
     if room_id not in range(1,11):
-        raise HTTPException(400, "room id doesn't exist.")
+        raise HTTPException(400, "room id should in range 1 to 10")
     reservation_list = []
-    for item in collection.find({"room_id": room_id}):
+    for item in collection.find({"room_id": room_id}, {"_id": False}):
         reservation_list.append(item)
-    return {"result": list(collection.find({"room_id": room_id}))}
+    return {"result": reservation_list}
 
 @app.post("/reservation")
 def reserve(reservation : Reservation):
-    if 
-    collection.update_one({"name": reservation.name, "start_date": reservation.start_date, "end_date": reservation.end_date, "room_id": reservation.room_id})
+    if reservation not in range(1,11):
+        raise HTTPException(400, "room id should in range 1 to 10")
+    if not room_avaliable(reservation.room_id, str(reservation.start_date), str(reservation.end_date)):
+        raise HTTPException(400, "room is unavailable")
+    if not reservation.start_date <= reservation.end_date:
+        raise HTTPException(400, "start date should be before end date")
+    collection.update_one({"name": reservation.name, "start_date": reservation.start_date.strftime("%Y-%m-%d"), "end_date": reservation.end_date.strftime("%Y-%m-%d"), "room_id": reservation.room_id})
     return {"msg": "already reserve"}
 
 @app.put("/reservation/update")
 def update_reservation(reservation: Reservation, new_start_date: date = Body(), new_end_date: date = Body()):
-    collection.update_one({"name": reservation.name, "start_date": reservation.start_date, "end_date": reservation.end_date, "room_id": reservation.room_id}, {"$set": {"start_date": new_start_date, "end_date": new_end_date}})
+    if not room_avaliable(reservation.room_id, str(reservation.start_date), str(reservation.end_date)):
+        raise HTTPException(400, "room is unavailable")
+    if not reservation.start_date <= reservation.end_date:
+        raise HTTPException(400, "start date should be before end date")
+    collection.update_one({"name": reservation.name, "start_date": reservation.start_date.strftime("%Y-%m-%d"), "end_date": reservation.end_date.strftime("%Y-%m-%d"), "room_id": reservation.room_id}, {"$set": {"start_date": new_start_date.isoformat(), "end_date": new_end_date.isoformat()}})
     return {"msg": "already update"}
 
 @app.delete("/reservation/delete")
 def cancel_reservation(reservation: Reservation):
     if collection.find_one({"id": reservation.room_id}):
         raise HTTPException(500, "Room id is not found")
-    collection.delete_one({"name": reservation.name, "start_date": reservation.start_date, "end_date": reservation.end_date, "room_id": reservation.room_id}) 
+    collection.delete_one({"name": reservation.name, "start_date": reservation.start_date.strftime("%Y-%m-%d"), "end_date": reservation.end_date.strftime("%Y-%m-%d"), "room_id": reservation.room_id}) 
     return {"msg": "already delete"}
 
 if __name__ == "__main__":
